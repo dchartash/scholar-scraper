d452b21f38c4351cf5348d6a8b11af0e18f3b9f2
dchartas <dchartas@archelon>
2016-12-02 23:15:35 -0500

    first pass of RG scrape

---
+++ stats-rg.js
@@ -0,0 +1,269 @@
+var date = "Fri Dec 02 2016 23:15:13 GMT-0500 (EST)"
+var data = [
+  {
+    "name": "Paul G Biondich",
+    "photo": "http://scholar.google.com/citations?view_op=view_photo&user=yDdCuNEAAAAJ&citpid=1",
+    "keywords": [
+      "Health Informatics"
+    ],
+    "affiliation": "Associate Professor of Pediatrics / Informatics",
+    "url": "https://scholar.google.com/citations?user=yDdCuNEAAAAJ",
+    "stats": {
+      "citations": [
+        "1390",
+        "1004"
+      ],
+      "hindex": [
+        "23",
+        "21"
+      ],
+      "i10index": [
+        "29",
+        "28"
+      ]
+    },
+    "year": "2004"
+  },
+  {
+    "name": "Paul Dexter",
+    "photo": "http://scholar.google.com/citations/images/avatar_scholar_150.jpg",
+    "keywords": [
+      "Medical Informatics",
+      "Clinical decision support"
+    ],
+    "affiliation": "Indiana University, Regenstrief Institute",
+    "url": "https://scholar.google.com/citations?user=6dncqlIAAAAJ",
+    "stats": {
+      "citations": [
+        "3465",
+        "1414"
+      ],
+      "hindex": [
+        "26",
+        "18"
+      ],
+      "i10index": [
+        "46",
+        "32"
+      ]
+    },
+    "year": "1995"
+  },
+  {
+    "name": "Brian E Dixon",
+    "photo": "http://scholar.google.com/citations?view_op=view_photo&user=E6ehgh4AAAAJ&citpid=2",
+    "keywords": [
+      "Public Health Informatics",
+      "Health Information Exchange",
+      "Clinical Decision Support",
+      "Health Informatics",
+      "Biomedical Informatics"
+    ],
+    "affiliation": "Associate Professor, Indiana University; Research Scientist, Regenstrief",
+    "url": "https://scholar.google.com/citations?user=E6ehgh4AAAAJ",
+    "stats": {
+      "citations": [
+        "680",
+        "631"
+      ],
+      "hindex": [
+        "15",
+        "13"
+      ],
+      "i10index": [
+        "21",
+        "20"
+      ]
+    },
+    "year": "2008"
+  },
+  {
+    "name": "Stephen M Downs",
+    "photo": "http://scholar.google.com/citations?view_op=view_photo&user=8Uxl3nsAAAAJ&citpid=1",
+    "keywords": [
+      "Pediatrics",
+      "Medical Informatics",
+      "Decision Sciences",
+      "Health Services Research"
+    ],
+    "affiliation": "Jean and Jerry Bepko Professor of Pediatrics, Indiana University School of Medicine",
+    "url": "https://scholar.google.com/citations?user=8Uxl3nsAAAAJ",
+    "stats": {
+      "citations": [
+        "3618",
+        "1950"
+      ],
+      "hindex": [
+        "33",
+        "25"
+      ],
+      "i10index": [
+        "72",
+        "55"
+      ]
+    },
+    "year": "1991"
+  },
+  {
+    "name": "JT Finnell",
+    "photo": "http://scholar.google.com/citations?view_op=view_photo&user=YN4SnHsAAAAJ&citpid=2",
+    "keywords": [
+      "Medical Informatics",
+      "Emergency Medicine",
+      "Operational Research",
+      "Clinical Decision Support",
+      "Education"
+    ],
+    "affiliation": "Indiana University",
+    "url": "https://scholar.google.com/citations?user=YN4SnHsAAAAJ",
+    "stats": {
+      "citations": [
+        "814",
+        "532"
+      ],
+      "hindex": [
+        "13",
+        "11"
+      ],
+      "i10index": [
+        "15",
+        "14"
+      ]
+    },
+    "year": "2004"
+  },
+  {
+    "name": "Shaun Grannis",
+    "photo": "http://scholar.google.com/citations?view_op=view_photo&user=f1znT0MAAAAJ&citpid=1",
+    "keywords": [
+      "Medical Informatics",
+      "Public Health Informatics",
+      "Clinical Decision Support",
+      "Syndromic Surveillance",
+      "Patient Matching"
+    ],
+    "affiliation": "Associate Professor of Family Medicine, Indiana University School of Medicine",
+    "url": "https://scholar.google.com/citations?user=f1znT0MAAAAJ",
+    "stats": {
+      "citations": [
+        "1707",
+        "1099"
+      ],
+      "hindex": [
+        "20",
+        "18"
+      ],
+      "i10index": [
+        "32",
+        "30"
+      ]
+    },
+    "year": "2003"
+  },
+  {
+    "name": "Timothy Imler",
+    "photo": "http://scholar.google.com/citations?view_op=view_photo&user=LbaJobgAAAAJ&citpid=1",
+    "keywords": [
+      "Gastroenterology",
+      "Biomedical Informatics"
+    ],
+    "affiliation": "Gastroenterology Fellow, Indiana University",
+    "url": "https://scholar.google.com/citations?user=LbaJobgAAAAJ",
+    "stats": {
+      "citations": [
+        "154",
+        "153"
+      ],
+      "hindex": [
+        "7",
+        "7"
+      ],
+      "i10index": [
+        "6",
+        "6"
+      ]
+    },
+    "year": "2012"
+  },
+  {
+    "name": "Burke Mamlin",
+    "photo": "http://scholar.google.com/citations?view_op=view_photo&user=r39o3wsAAAAJ&citpid=1",
+    "keywords": [
+      "Medical Informatics",
+      "Open Source"
+    ],
+    "affiliation": "Associate Professor of Clinical Medicine, Indiana University",
+    "url": "https://scholar.google.com/citations?user=r39o3wsAAAAJ",
+    "stats": {
+      "citations": [
+        "2111",
+        "1199"
+      ],
+      "hindex": [
+        "24",
+        "20"
+      ],
+      "i10index": [
+        "31",
+        "29"
+      ]
+    },
+    "year": "1990"
+  },
+  {
+    "name": "J Marc Overhage",
+    "photo": "http://scholar.google.com/citations?view_op=view_photo&user=eWfryRwAAAAJ&citpid=2",
+    "keywords": [
+      "clinical decision support",
+      "cpoe",
+      "computerized provider order entry",
+      "health information exchange",
+      "ade"
+    ],
+    "affiliation": "Chief Medical Informatics Officer, Cerner Health Services; [former] Regenstrief Professor of",
+    "url": "https://scholar.google.com/citations?user=eWfryRwAAAAJ",
+    "stats": {
+      "citations": [
+        "11690",
+        "5030"
+      ],
+      "hindex": [
+        "51",
+        "36"
+      ],
+      "i10index": [
+        "119",
+        "83"
+      ]
+    },
+    "year": "1993"
+  },
+  {
+    "name": "Titus Schleyer",
+    "photo": "http://scholar.google.com/citations?view_op=view_photo&user=5LCpwUsAAAAJ&citpid=1",
+    "keywords": [
+      "biomedical informatics",
+      "HealthIT",
+      "electronic health records",
+      "medicine",
+      "dentistry"
+    ],
+    "affiliation": "Clem McDonald Professor of Biomedical Informatics",
+    "url": "https://scholar.google.com/citations?user=5LCpwUsAAAAJ",
+    "stats": {
+      "citations": [
+        "1959",
+        "1075"
+      ],
+      "hindex": [
+        "25",
+        "18"
+      ],
+      "i10index": [
+        "54",
+        "34"
+      ]
+    },
+    "year": "1999"
+  }
+];
